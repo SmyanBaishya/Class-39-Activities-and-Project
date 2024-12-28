66133a3e269bba12dc6a8664797c7f64c1b7d2fa
SmyanBaishya <smyanbaishya20@gmail.com>
2024-12-28 16:11:31 +0530

    Activity1.py

--- Activity1.py
+++ Activity1.py
@@ -4,3 +4,10 @@ window = tk.Tk()
 
 window.title('Test')
 window.geometry('400x400')
+hello = tk.Label(text = "Hello World")
+hello.pack()
+button = tk.Button(text = "Click me!")
+button.pack()
+tk.mainloop()
+
+# ACTIVITY DOES NOT RUN ON VSC, OPEN REPLIT TO RUN
